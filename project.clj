099d789770d8579668cd030d9c39b58938cdf270
Adam Frey <adam@adamfrey.me>
2022-07-11 09:59:40 -0400

    Tag project as 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.afrey/lein-google-cloud-artifact-registry "1.0.0-rc.1"
+(defproject org.clojars.afrey/lein-google-cloud-artifact-registry "1.0.0"
   :description "A leiningen plugin that enables fetching dependencies from Google
   Cloud Artifact Registry maven repositories."
   :url "https://github.com/AdamFrey/lein-google-cloud-artifact-registry"
